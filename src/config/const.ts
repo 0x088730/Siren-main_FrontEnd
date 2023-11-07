c47e231bc9a5ebe1145059c4d1b72bbd65a2de96
waxp2edev0101 <jonathanross0555@gmail.com>
2023-11-07 12:21:19 +0100

    Siren_main_game

---
+++ src/config/const.ts
@@ -0,0 +1,4 @@
+export const SIREN_SPINE: string = 'siren-spine'
+export const ENEMY_SPINE: string = 'enemy-spine'
+export const SIREN_SPINE_FIRE: string = 'siren-spine-fire'
+export const SIREN_SPINE_FTHROW: string = 'siren-spin-fthrow'
